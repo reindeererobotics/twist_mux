4f40b3fed3a20ca5c8e3d562450dd292c964064f
Joseph Ojo <joe@reindeere.ca>
2025-02-16 15:25:12 -0500

    feat: refined config and topics for dasher

--- launch/twist_mux_launch.py
+++ launch/twist_mux_launch.py
@@ -66,19 +66,20 @@ def generate_launch_description():
             package='twist_mux',
             executable='twist_marker',
             output='screen',
-            remappings={('/twist', LaunchConfiguration('cmd_vel_out'))},
+            remappings={('/twist', LaunchConfiguration('cmd_vel_out')), 
+                        ('/marker', '/twistmux_marker')},
             parameters=[{
                 'use_sim_time': LaunchConfiguration('use_sim_time'),
                 'frame_id': 'base_link',
                 'scale': 1.0,
                 'vertical_position': 2.0}]),
-        Node(
-            package='twist_mux',
-            executable='joystick_relay.py',
-            output='screen',
-            remappings={('joy_vel_in', 'input_joy/cmd_vel'),
-                        ('joy_vel_out', 'joy_vel')},
-            parameters=[
-                {'use_sim_time': LaunchConfiguration('use_sim_time')},
-                LaunchConfiguration('config_joy')])
+        # Node(
+        #     package='twist_mux',
+        #     executable='joystick_relay.py',
+        #     output='screen',
+        #     remappings={('joy_vel_in', 'input_joy/cmd_vel'),
+        #                 ('joy_vel_out', 'joy_vel')},
+        #     parameters=[
+        #         {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        #         LaunchConfiguration('config_joy')])
     ])
